047b66db33d30019bc84e5179a232f520e17c973
claire-shin <tmdghk05@gmail.com>
2020-04-10 19:12:39 +0900

    change color

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,30 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
 
+Wordpress2016.googleFonts = [
+  {
+    name: "Noto Sans KR",
+    styles: ["100", "300", "400", "500", "700", "900"],
+  },
+];
+Wordpress2016.headerFontFamily = ["Noto Sans KR", "sans-serif"];
+Wordpress2016.bodyFontFamily = ["Noto Sans KR", "sans-serif"];
 Wordpress2016.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
     },
+    "a": {
+      color: '#d9bf77'
+    },
+    "h3>a" : {
+      color: '#639a67'
+    },
+    "article>header>h1" : {
+      color: '#639a67'
+    }
   }
 }
-
-delete Wordpress2016.googleFonts
 
 const typography = new Typography(Wordpress2016)
 
